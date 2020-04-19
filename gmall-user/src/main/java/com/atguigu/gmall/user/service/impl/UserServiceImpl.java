ac16c58ebb6f1516b568b4f6e438199dc3b6d792
wangyi <wangyi394527@163.com>
2020-04-19 10:20:31 +0800

    user测试

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+        List<UmsMember> umsMembers = userMapper.selectAll();
+        return umsMembers;
+    }
+}
